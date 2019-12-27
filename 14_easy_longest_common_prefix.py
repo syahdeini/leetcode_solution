39baf8e476bd3e5ae49dd7542c921d8b22b4b041
Aldy syahdeini <syahdeini@gmail.com>
2019-12-27 10:13:27 +0100

    Create 14_easy_longest_common_prefix.py

---
+++ 14_easy_longest_common_prefix.py
@@ -0,0 +1,19 @@
+# 10:12 27-12-2019
+# https://leetcode.com/problems/palindrome-number/
+# A Syahdeini
+# Lame solution O(n^2)
+class Solution:
+    
+    def match(self, str_a, str_b):
+        for idx,char_a in enumerate(str_a):
+            if (idx>(len(str_b)-1)):
+                return str_b
+            if char_a != str_b[idx]:
+                return str_b[:idx]
+        return str_a
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs)==0: return ""
+        prefix = strs[0]
+        for _str in strs[1:]:
+            prefix = self.match(prefix,_str)
+        return prefix if prefix else ""
